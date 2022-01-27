a3130c5422b01287e6fa4a2cefddce3b8cb75e7e
castrorestrepo <julianandresrestrepocastro@gmail.com>
2022-01-27 08:47:25 -0500

    se modifica el header, se adiciona indicacion de version 2 y tipo de empresa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 export default function Header() {
     return (
-
+        <div>
             <h1 className="text-3xl text-center md:w-2/3 mx-auto">
-                Seguimineto de pacientes {""}<br></br>
-            <span className="text-yellow-300"> Veterinaria</span> 
+                Seguimiento de pacientes verion 2.0 {""}<br></br>
+            <span className="text-yellow-300"> Veterinaria S.A.S </span> 
+
             </h1>
+        </div>
+            
      
     )
 }
